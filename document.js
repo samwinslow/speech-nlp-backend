e7ac36579773f06da68eda985bfe6685d327e2ff
Sam Winslow <sammywinslow@gmail.com>
2020-09-01 22:05:59 -0400

    doc.get

--- document.js
+++ document.js
@@ -7,14 +7,22 @@ const headers = {
   'Access-Control-Allow-Credentials': true,
 }
 
-export function create(event, context, callback) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
-  const data = JSON.parse(event.body)
+function HTTPError(error) {
+  console.log('Error: ', JSON.stringify(error))
+  return {
+    statusCode: error.statusCode || 500,
+    headers: headers,
+    body: JSON.stringify({ status: false })
+  }
+}
 
+export function create(event, context, callback) {
+  // Creates document for specified key with content.
+  const data = JSON.parse(event.body)
   const params = {
     TableName: process.env.NOSQL_TABLE_NAME,
     Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
+      userId: event.requestContext.authorizer.claims.sub,
       noteId: uuid.v1(),
       content: data.content,
       createdAt: Date.now()
@@ -22,30 +30,44 @@ export function create(event, context, callback) {
   }
 
   dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    }
-
-    // Return status code 500 on error
+    // Error
     if (error) {
-      console.log('Error: ', JSON.stringify(error))
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      }
+      const response = HTTPError(error)
       callback(null, response)
       return
     }
-
-    // Return status code 200 and the newly created item
-    console.log('Anything.', JSON.stringify({ event, context, callback }))
+    // Success
     const response = {
       statusCode: 200,
       headers: headers,
       body: JSON.stringify(params.Item)
+    }
+    return callback(null, response)
+  })
+}
+
+export function get(event, context, callback) {
+  // Gets document content for specified key.
+  const params = {
+    TableName: process.env.NOSQL_TABLE_NAME,
+    Key: {
+      userId: event.requestContext.authorizer.claims.sub,
+      noteId: event.pathParameters.id,
+    }
+  }
+
+  dynamoDb.get(params, (error, data) => {
+    // Error
+    if (error) {
+      const response = HTTPError(error)
+      callback(null, response)
+      return
+    }
+    // Success
+    const response = {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify(data.Item)
     }
     return callback(null, response)
   })
