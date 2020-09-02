f1db26320ab1648b425d047e397316b3b5ea7a51
Sam Winslow <sammywinslow@gmail.com>
2020-09-01 22:26:58 -0400

    Update endpoint ✅

--- document.js
+++ document.js
@@ -72,3 +72,63 @@ export function get(event, context, callback) {
     return callback(null, response)
   })
 }
+
+export function getAll(event, context, callback) {
+  // Gets document content for specified key.
+  const params = {
+    TableName: process.env.NOSQL_TABLE_NAME,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': event.requestContext.authorizer.claims.sub
+    }
+  }
+
+  dynamoDb.query(params, (error, data) => {
+    // Error
+    if (error) {
+      const response = HTTPError(error)
+      callback(null, response)
+      return
+    }
+    // Success
+    const response = {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify(data.Items)
+    }
+    return callback(null, response)
+  })
+}
+
+export function update(event, context, callback) {
+  // Gets document content for specified key.
+  const data = JSON.parse(event.body)
+  const params = {
+    TableName: process.env.NOSQL_TABLE_NAME,
+    Key: {
+      userId: event.requestContext.authorizer.claims.sub,
+      noteId: event.pathParameters.id,
+    },
+    UpdateExpression: "SET content = :content",
+    ExpressionAttributeValues: {
+      ":content": data.content || null
+    },
+    ReturnValues: "ALL_NEW"
+  }
+
+  dynamoDb.update(params, (error, data) => {
+    // Error
+    if (error) {
+      const response = HTTPError(error)
+      callback(null, response)
+      return
+    }
+    // Success
+    const response = {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify({ status: true })
+    }
+    return callback(null, response)
+  })
+}
