ce5f290e83566d698a0727faa18c6bbfc1a9ce57
Sam Winslow <sammywinslow@gmail.com>
2020-09-01 21:10:03 -0400

    es6 syntax

--- document.js
+++ document.js
@@ -1,13 +1,13 @@
 const uuid = require('uuid')
 const AWS = require('aws-sdk')
 
-// const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Credentials': true,
 }
 
-exports.create = (event, context, callback) => {
+export function create(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body)
 
@@ -21,31 +21,32 @@ exports.create = (event, context, callback) => {
     }
   }
 
-  // dynamoDb.put(params, (error, data) => {
-  //   // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-  //   const headers = {
-  //     "Access-Control-Allow-Origin": "*",
-  //     "Access-Control-Allow-Credentials": true
-  //   }
+  dynamoDb.put(params, (error, data) => {
+    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    }
 
-  //   // Return status code 500 on error
-  //   if (error) {
-  //     const response = {
-  //       statusCode: 500,
-  //       headers: headers,
-  //       body: JSON.stringify({ status: false })
-  //     }
-  //     callback(null, response)
-  //     return
-  //   }
+    // Return status code 500 on error
+    if (error) {
+      console.log('Error: ', JSON.stringify(error))
+      const response = {
+        statusCode: 500,
+        headers: headers,
+        body: JSON.stringify({ status: false })
+      }
+      callback(null, response)
+      return
+    }
 
-  //   // Return status code 200 and the newly created item
-  console.log('Anything.', JSON.stringify({ event, context, callback }))
+    // Return status code 200 and the newly created item
+    console.log('Anything.', JSON.stringify({ event, context, callback }))
     const response = {
       statusCode: 200,
       headers: headers,
       body: JSON.stringify(params.Item)
     }
     return callback(null, response)
-  // })
+  })
 }
